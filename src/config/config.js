9f3d93c21dac5417db1d585b57a210e3e4c7920b
Melisa <melisazatta@hotmail.com>
2026-06-08 23:52:06 -0300

    config

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,7 @@ dotenv.config()
 const config = {
     nodeEnv: process.env.NODE_ENV || "development",
     port: process.env.PORT || 8080,
-    mongoUri: process.env.MONGO_URI 
-    // || "mongodb+srv://melisazattaCoder:passwordfacil@cluster0.e0ow4zb.mongodb.net/ecommerce?appName=Cluster0"
+    mongoUri: process.env.MONGO_URI
 }
 
 export default config
